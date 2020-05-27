92fd971ad26fe9f00b16a74cf962fc71176303db
JISU LEE <jsnote2013@gmail.com>
2020-05-27 21:48:31 +0900

    첫번째 액티비티에서 메인으로 돌아가기 - finish 기능 활용

--- app/src/main/java/com/jisu/intentpractice_20200527/FirstActivity.kt
+++ app/src/main/java/com/jisu/intentpractice_20200527/FirstActivity.kt
@@ -1,12 +1,23 @@
 package com.jisu.intentpractice_20200527
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        goBackBtn.setOnClickListener {
+//             돌아갈 때는 Intent 로 돌아가지 않는다.
+//             Intent는 새로 화면을 만들어내는 효과가 있다.
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+        }
     }
 }
