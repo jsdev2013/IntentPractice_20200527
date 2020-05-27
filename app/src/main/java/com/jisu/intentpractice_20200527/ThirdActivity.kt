bfcbce59b24fe2b9cb24be439208fc9b2df0a5fa
JISU LEE <jsnote2013@gmail.com>
2020-05-27 22:16:44 +0900

    첨부된 내용을 받아서 출력

--- app/src/main/java/com/jisu/intentpractice_20200527/ThirdActivity.kt
+++ app/src/main/java/com/jisu/intentpractice_20200527/ThirdActivity.kt
@@ -2,11 +2,16 @@ package com.jisu.intentpractice_20200527
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val content = intent.getStringExtra("input")
+
+        contentTxt.text = content
     }
 }
