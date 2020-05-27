c14d87f5b8e34e1f192dc06c9cacfcdd343bc5e5
JISU LEE <jsnote2013@gmail.com>
2020-05-27 22:03:54 +0900

    메인에서 입력했던 내용을 서브로 가져오기

---
+++ app/src/main/java/com/jisu/intentpractice_20200527/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.jisu.intentpractice_20200527
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
